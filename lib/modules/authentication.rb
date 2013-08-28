7e7d6bd05ef92fbf9689c297705ba3e7e431ce59
Yulia Oletskaya <voletsky@gmail.com>
2013-08-28 17:46:33 +0300

    Login Screen template finished

--- lib/modules/authentication.rb
+++ lib/modules/authentication.rb
@@ -1,6 +1,6 @@
 module Authentication
   def authenticate(warden_email, warden_password)
     user = where(["lower(email) = :email", { :email => warden_email.strip.downcase }]).first
-    user.valid_password?(warden_password) ? user : nil
+    user.try(:valid_password?, warden_password) ? user : nil
   end
 end
