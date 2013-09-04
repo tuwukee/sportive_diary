07be806ddebbc8f8cdb37cd14d8889122988706a
Yulia Oletskaya <voletsky@gmail.com>
2013-09-04 18:04:59 +0300

    flash messages

--- app/controllers/parents/registrations_controller.rb
+++ app/controllers/parents/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Parents::RegistrationsController < Devise::RegistrationsController
   def create
     super
-    session[:class_name] = resource.class.name
+    session[:class_name] = resource.class.name unless resource.invalid?
   end
 end
